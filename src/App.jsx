c32504ef1dcadd4f808ddb1f69c230b9cd7fe2e1
Christian <christian.espinoza1008@gmail.com>
2022-11-22 14:57:13 -0500

    Add content about useSate video 2 to 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,18 @@
-import { Counter } from "./components/Counter";
-import Greet from "./components/Greet";
-import { GreetProps } from "./components/GreetProps";
-import Message from "./components/Message";
-import NameList from "./components/NamesList";
-import UserGreeting from "./components/UserGreeting";
-import Welcome from "./components/Welcome";
-import WelcomeProps from "./components/WelcomeProps";
-import Stylesheet from "./components/Stylesheet";
-import Inline from "./components/Inline";
-import "./appStyles.css";
-import styles from "./appStyles.module.css";
-import Form from "./components/Form";
-import LifecycleA from "./components/LifecycleA";
-import FragmentDemo from "./components/FragmentDemo";
-import Table from "./Table";
-import StateLab from "./components/StateLab";
-import ParenComp from "./components/ParentComp";
-import RefsDemo from "./components/RefsDemo";
-import FRInput from "./components/FRInputs";
-import FRParentInput from "./components/FRParentInput";
-import PortalDemo from "./components/PortalDemo";
-import Hero from "./components/Hero";
-import ErrorBoundary from "./components/ErrorBoundary";
-import ClickCounter from "./components/ClickCounter";
-import HoverCounter from "./components/HoverCounter";
-import ClickCounterTwo from "./components/ClickCounterTwo";
-import HoverCounterTwo from "./components/HoverCounterTwo";
-import User from "./components/User";
-import Counter2 from "./components/Counter2";
-import { UserProvider } from "./components/UserContext";
-import ComponentC from "./components/ComponentC";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
+import React from "react";
+import ClassCounter from "./components2/ClassCounter";
+import HookCounter from "./components2/HookCounter";
+import HookCounterFour from "./components2/HookCounterFour";
+import HookCounterThree from "./components2/HookCounterThree";
+import HookCounterTwo from "./components2/HookCounterTwo";
+
 function App() {
 	return (
 		<div className="App">
-			{/* <Form /> */}
-			{/* <LifecycleA /> */}
-			{/* <FragmentDemo /> */}
-			{/* <Table /> */}
-			{/* <StateLab /> */}
-			{/* <ParenComp /> */}
-			{/* <RefsDemo /> */}
-			{/* <FRParentInput /> */}
-			{/* <PortalDemo value={"Hello World from portal"} /> */}
-			{/* <ErrorBoundary>
-				<Hero heroName="Batman" />
-			</ErrorBoundary>
-			<ErrorBoundary>
-				<Hero heroName="Superman" />
-			</ErrorBoundary>
-			<ErrorBoundary>
-				<Hero heroName="Joker" />
-			</ErrorBoundary> */}
-			{/* <ClickCounter name="vishwas" />
-			<HoverCounter name="vishwas" />
-			 <ClickCounterTwo />
-			<HoverCounterTwo /> */}
-			{/* <User render={(isLoggedIn) => (isLoggedIn ? "vishwas" : "Guest")} /> */}
-			{/* <Counter2
-				render={(count, incrementCount) => (
-					<ClickCounterTwo
-						count={count}
-						incrementCount={incrementCount}
-					/>
-				)}
-			/>
-			<Counter2
-				render={(count, incrementCount) => (
-					<HoverCounterTwo
-						count={count}
-						incrementCount={incrementCount}
-					/>
-				)}
-			/> */}
-			{/* <UserProvider value="Vishwas">
-				<ComponentC />
-			</UserProvider> */}
-			{/* <PostList /> */}
-			<PostForm />
+			<ClassCounter />
+			<HookCounter />
+			<HookCounterTwo />
+			<HookCounterThree />
+			<HookCounterFour />
 		</div>
 	);
 }
---
+++ src/components2/HookCounter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+function HookCounter() {
+	const [count, setCount] = useState(0);
+	const incrementCount = () => setCount(count + 1);
+	return (
+		<div>
+			<button onClick={incrementCount}>Count {count}</button>
+		</div>
+	);
+}
+
+export default HookCounter;
